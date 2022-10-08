29fd24aadfbdeaec7f3bebb0da03ab97ffb1f159
Fatih Erdoğan <fatiherdogann@yandex.com.tr>
2022-10-08 08:35:20 +0300

    Entity Framework Udemy Education

---
+++ EFCoreLessons/DbContextProperties/Migrations/20220909110716_initial.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DbContextProperties.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EducationsWithDAAttributes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Name = table.Column<string>(type: "varchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EducationsWithDAAttributes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InvoiceWithFluentAPI",
+                columns: table => new
+                {
+                    Invoice_Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InvoiceNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TranDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nchar(150)", fixedLength: true, maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InvoiceWithFluentAPI", x => x.Invoice_Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductsItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Stock = table.Column<int>(type: "int", nullable: false),
+                    Barcode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductsItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EducationsWithDAAttributes");
+
+            migrationBuilder.DropTable(
+                name: "InvoiceWithFluentAPI");
+
+            migrationBuilder.DropTable(
+                name: "ProductsItem");
+        }
+    }
+}
---
+++ EFCoreLessons/DbContextProperties/Program.cs
@@ -0,0 +1,62 @@
+﻿using DbContextProperties;
+using Microsoft.EntityFrameworkCore;
+
+Initializer.Build();
+
+using (var _context = new AppDbContext())
+{
+
+    #region Default data çekme --track etmeme özelliği
+    // AsNoTracking ef core un dataları track etmemesini sağlıyor.Memory yönetimi için
+    var products = await _context.ProductsItem.AsNoTracking().ToListAsync();
+
+    products.ForEach(x =>
+    {
+
+        var state = _context.Entry(x).State;
+        Console.WriteLine(x.Id + "- " + x.Name + " : " + x.Price + " - " + x.Stock + "--> State: " + state);
+    });
+    #endregion
+
+    #region CreatedDate SaveChanges metodu override edilerek tüm kayıtlara verildi.Merkezi yapı kuruldu
+    _context.ProductsItem.Add(new Product { Name = "Masa", Price = 4555, Stock = 111, Barcode = "msmsms" });
+    _context.ProductsItem.Add(new Product { Name = "Sandalye", Price = 111, Stock = 222, Barcode = "ssdsdssd" });
+    _context.ProductsItem.Add(new Product { Name = "Yastık", Price = 32, Stock = 655, Barcode = "ysysysy" });
+    _context.SaveChanges();
+    #endregion
+
+    #region ContextId property
+    Console.WriteLine($"Context Id: {_context.ContextId}");
+    #endregion
+
+    #region Database property --veritabanı işlemlerini yapmak için kullanılır.
+    var timeout =_context.Database.GetCommandTimeout();
+    Console.WriteLine($"Command Timeout: {timeout}");
+    #endregion
+
+
+
+    #region DbSet properties
+    //ilk kaydı getirir.kayıt yoksa hata alır.
+    var firstItem =_context.ProductsItem.First(x=>x.Id>100);
+
+    //ilk kaydı getirir.kayıt yoksa default değer döner. Default değer değiştirilebilir.
+    var firstDefaultItem = _context.ProductsItem.FirstOrDefault(x => x.Id == 100,new Product { Name="Boş Ürün"});
+
+    //şayet eşleşen tek kayıt varsa çalışır.Birden fazla kayıtta yada kayıt yoksa hata fırlatır.
+    var singleItem = _context.ProductsItem.Single(x=>x.Stock>1);
+
+    //birden fazla kayıt varsa hata alır.Kayıt yoksa default değeri döner.
+    var singleDefaultItem = _context.ProductsItem.SingleOrDefault(x => x.Stock > 1,new Product { Name="Boş Ürün"});
+
+    //koşulu sağlayan dataları döner.
+    var whereItem = _context.ProductsItem.Where(x=>x.Price>100 && x.CreatedDate==null).ToList();
+
+    // find primary keye göre ilgili datayı getirir. Alternatif olarak single ve find kullanılabilir.Bulamazsa null döner.
+    var findItem = _context.ProductsItem.Find(10);
+    var findfirstItem = _context.ProductsItem.First(x => x.Id ==10);
+    var singlefirstItem = _context.ProductsItem.Single(x => x.Id == 10);
+    #endregion
+
+    Console.ReadLine();
+}
